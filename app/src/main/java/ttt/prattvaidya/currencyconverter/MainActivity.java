67baad65f3c0967092a30329b85a0e51baeef414
Pratt Vaidya <pratt.vaidya@gmail.com>
2021-07-23 11:18:18 -0700

    Add logic to convert from CAD to USD

--- app/src/main/java/ttt/prattvaidya/currencyconverter/MainActivity.java
+++ app/src/main/java/ttt/prattvaidya/currencyconverter/MainActivity.java
@@ -3,6 +3,10 @@ package ttt.prattvaidya.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void handleConvert(View view) {
+        EditText editText = (EditText) findViewById(R.id.editTextNumber);
+        String amountInCAD = editText.getText().toString();
+        double amtCAD = Double.parseDouble(amountInCAD);
+        double amtUSD = amtCAD * 0.8;
+//        String amountInUSD = Double.toString(amtUSD);
+        String amountInUSD = String.format("%.2f",amtUSD);
+        Toast.makeText(this, "Amount in USD: " + amountInUSD, Toast.LENGTH_SHORT).show();
+        Log.i("action", "Amount in USD: " + amountInUSD);
     }
 }
